7edb5e313e0d0b3b59acaed66575502ad4e0d0e0
Henrik Nortamo <you@example.com>
2022-02-23 21:50:54 +0200

    Scale resources

--- Workflow/SnakeMake/run.sh
+++ Workflow/SnakeMake/run.sh
@@ -5,4 +5,4 @@ export PERMEDCOE_ASSETS=$(readlink -f $(pwd)/../../../BuildingBlocks/Resources/a
 
 export _DATA_DIR=$(readlink -f ../../Resources/data/)
 export SINGULARITY_BIND="$PERMEDCOE_ASSETS:$PERMEDCOE_ASSETS,$_DATA_DIR:$_DATA_DIR"
-snakemake --cores 12 meta_analysis 
+snakemake --cores 10 meta_analysis 
