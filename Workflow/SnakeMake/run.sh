0d4404c3fbdd3f309ebaec123d52fd4b3131b21a
Henrik Nortamo <you@example.com>
2022-02-23 21:18:46 +0200

    Enable snakemake to run workflow
    The following are bugs/strange things and should be fixed
       - Some BBs require passing -c argument as otherwise an error is thrown even if the config is not needed
       - Some BBs require adding a positional parameter indicating the parallelism

--- Workflow/SnakeMake/run.sh
+++ Workflow/SnakeMake/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+
 export PERMEDCOE_IMAGES=$(readlink -f $(pwd)/../../../BuildingBlocks/Resources/images/)/
 export PERMEDCOE_ASSETS=$(readlink -f $(pwd)/../../../BuildingBlocks/Resources/assets/)/
 
 export _DATA_DIR=$(readlink -f ../../Resources/data/)
 export SINGULARITY_BIND="$PERMEDCOE_ASSETS:$PERMEDCOE_ASSETS,$_DATA_DIR:$_DATA_DIR"
-snakemake --cores 12 --cluster-config cluster.json --cluster "sbatch -A {cluster.account} -p {cluster.partition} -n {cluster.n}  -t {cluster.time} --mem {cluster.memory}"  meta_analysis
+snakemake --cores 12 meta_analysis 
