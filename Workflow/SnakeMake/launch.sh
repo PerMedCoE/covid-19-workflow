f1748a3e10faa99da490ba8319b934e1c3d61622
Henrik Nortamo <you@example.com>
2022-02-23 21:50:41 +0200

    Add more proper slurm example

--- Workflow/SnakeMake/launch.sh
+++ Workflow/SnakeMake/launch.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --cores=10
+#SBATCH --partition=test
+#SBATCH --time=00:15:00
+
 export PERMEDCOE_IMAGES=$(readlink -f $(pwd)/../../../BuildingBlocks/Resources/images/)/
 export PERMEDCOE_ASSETS=$(readlink -f $(pwd)/../../../BuildingBlocks/Resources/assets/)/
 
 export _DATA_DIR=$(readlink -f ../../Resources/data/)
 export SINGULARITY_BIND="$PERMEDCOE_ASSETS:$PERMEDCOE_ASSETS,$_DATA_DIR:$_DATA_DIR"
-snakemake --cores 12 --cluster-config cluster.json --cluster "sbatch -A {cluster.account} -p {cluster.partition} -n {cluster.n}  -t {cluster.time} --mem {cluster.memory}"  meta_analysis
+snakemake --cores 10 meta_analysis 
