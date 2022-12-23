40150a9109288d1b46f4dfdead1794200a12bd6f
fconejer <francisco.conejero@bsc.es>
2022-12-23 11:58:48 +0100

    Updated BB installer.

---
+++ BuildingBlocks/from_local/clean_BBs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo "Cleaning covid-19-workflow required Building Blocks... Please wait..."
+
+CURRENT_DIR=$(pwd)
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd ../../../BuildingBlocks
+
+cd MaBoSS
+./clean.sh
+cd ..
+
+cd meta_analysis
+./clean.sh
+cd ..
+
+cd personalize_patient
+./clean.sh
+cd ..
+
+cd PhysiBoSS
+./clean.sh
+cd ..
+
+cd single_cell_processing
+./clean.sh
+cd ..
+
+cd ${CURRENT_DIR}
